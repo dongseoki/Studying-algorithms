50ea10e0ed66b30a90de052b5d5f0d061e8d3bb7
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-03-08 07:21:12 +0900

    fixed untracked files

--- java_programming/src/main/java/thisiscodingtestwithpython/c05dfsbfs/StackQueueRecursion.java
+++ java_programming/src/main/java/thisiscodingtestwithpython/c05dfsbfs/StackQueueRecursion.java
@@ -1,2 +1,19 @@
-package thisiscodingtestwithpython.c05dfsbfs;public class StackQueueRecursion {
+package thisiscodingtestwithpython.c05dfsbfs;
+
+import java.util.Stack;
+
+public class StackQueueRecursion {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        // stack
+        Stack<Integer> s = new Stack<>();
+        for(int i =0;i<5;i++){
+            s.push(i);
+        }
+        s.pop();
+        s.forEach(item-> System.out.println("item = " + item));
+
+    }
 }
