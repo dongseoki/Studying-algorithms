2fbaeec00ac74e85449fa3445f92c0ec9cf31aed
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-08-15 13:44:42 +0900

    feat: 아이템 줍기 완료.

--- java_programming/src/main/java/programmers/아이템_줍기/Solution.java
+++ java_programming/src/main/java/programmers/아이템_줍기/Solution.java
@@ -11,29 +11,187 @@ package programmers.아이템_줍기;
 // 시간 복잡도를 슥 계산해봤는데, 최대 방문한 점들이 20 * 20 사각형을 그린다고 가정했을때, 지점 80개, 그리고 방향 4개. 그리고 각각에 대해서 사각형 확인 4개.
 // 1280 충분히 작다 가능성.
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
+import java.util.*;
+import java.util.List;
+class Point implements  Comparable<Point>{
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        if (this.getX() != p.getX())
+            return this.getX() - p.getX();
+        else
+            return this.getY() - p.getY();
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
 class Solution {
-    Set<String> visitedEdgeSet  = new HashSet<String>();
-    List<Integer> candidateAnswerList = new ArrayList<>();
-    // 데이터 (X1 Y1) 와 (X2 Y2)  에 대해서
-    // 단순 문자열 정렬한 것들중 우선순위의 점을 이용하여
-    // (X1 Y1|X2 Y2) 를 문자열로 저장.(괄호 는 뺴고 생각.)
+
+
+    static int[] dRow = {-1,1,0,0};
+    static int[] dCol = {0,0,-1,+1};
+
+
+
+
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        List<String> availableEdgeList = findAvailableEdgeList(characterX, characterY);
-        String nextPosStr = getNextPos(availableEdgeList.get(0));
-        visitedEdgeSet.add(availableEdgeList.get(0));
-        tryOneCircleVisit(Integer.parseInt(nextPosStr.split(" ")[0]),Integer.parseInt(nextPosStr.split(" ")[1]));
+
+        answer = tryOneCircleVisit(rectangle, characterX,characterY,itemX,itemY);
 
         return answer;
     }
 
-    private void tryOneCircleVisit(int curX, int curY) {
-        tryOneCircleVisit(s, );
+    private int tryOneCircleVisit(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        Set<Point> visitedPointSet  = new HashSet<>();
+        // 데이터 (X1 Y1) 와 (X2 Y2)  에 대해서
+        // 단순 문자열 정렬한 것들중 우선순위의 점을 이용하여
+        // (X1 Y1|X2 Y2) 를 문자열로 저장.(괄호 는 뺴고 생각.)
+        List<Integer> candidateAnswerList = new ArrayList<>();
+
+        Point firstPoint = new Point(characterX, characterY);
+
+
+        visitedPointSet.add(firstPoint); //
+        Queue<Point> queue = new LinkedList<>();
+
+        queue.add(firstPoint);
+
+        Point itemPoint = new Point(itemX, itemY);
+
+        while(!queue.isEmpty()){
+            Point curPoint = queue.poll();
+            if(curPoint.equals(itemPoint)){
+                candidateAnswerList.add(visitedPointSet.size()-1);
+            }
+
+            //
+
+            List<Point> availablePointList = findAvailablePointList(curPoint);
+            for (Point p:availablePointList
+                 ) {
+                if (visitedPointSet.contains(p) == false && itItFrame(curPoint, p, rectangle)==true){
+                    visitedPointSet.add(p);
+                    queue.add(p);
+                    break;
+                }
+            }
+        }
+
+        // not visted First Point so...
+        int frameLength = visitedPointSet.size();
+        candidateAnswerList.add(frameLength - candidateAnswerList.get(0));
+        return Collections.min(candidateAnswerList);
+    }
+
+    private boolean itItFrame(Point p1, Point p2, int[][] rectangle) {
+        for (int[] recItem:rectangle
+             ) {
+            if(isItEdgeOfRec(recItem, p1, p2)){
+                for (int[] recItem2:rectangle
+                     ) {
+                    if (isItIncluded(recItem2,p1,p2))
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isItIncluded(int[] recItem, Point p1, Point p2) {
+        if(isThisPointInsideFrame(recItem, ((float)p1.getX()+p2.getX())/2, ((float)p1.getY()+p2.getY())/2))
+            return true;
+//        if(isThisPointInsideFrame(recItem, p1) && isThisPointOnFrame(recItem, p2))
+//            return true;
+//        if(isThisPointInsideFrame(recItem, p2) && isThisPointOnFrame(recItem, p1))
+//            return true;
+        return false;
+    }
+
+    private boolean isThisPointInsideFrame(int[] recItem, float mX, float mY) {
+        if (recItem[0]< mX && mX <recItem[2]&&
+                recItem[1]<mY && mY<recItem[3]){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isItEdgeOfRec(int[] recItem, Point p1, Point p2) {
+        if(isThisPointOnFrame(recItem, p1) && isThisPointOnFrame(recItem, p2))
+            return true;
+        return false;
+
+
+    }
+
+    private boolean isThisPointOnFrame(int[] recItem, Point p1) {
+        if (recItem[0]<= p1.getX() && p1.getX() <=recItem[2]&&
+                recItem[1]<=p1.getY() && p1.getY()<=recItem[3]){
+            if (p1.getX() == recItem[0] || p1.getX() == recItem[2])
+                return true;
+            if (p1.getY() == recItem[1] || p1.getY() == recItem[3])
+                return true;
+        }
+        return false;
+    }
+
+    private List<Point> findAvailablePointList(Point curPoint) {
+        List<Point> aPointList = new ArrayList<>();
+        for (int idx = 0; idx < dRow.length; idx++) {
+            aPointList.add(new Point(curPoint.getX() + dRow[idx], curPoint.getY() + dCol[idx]));
+        }
+
+        return aPointList;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[][]{{1, 1, 8, 4}, {2, 2, 4, 9}, {3, 6, 9, 8}, {6, 3, 7, 7}}, 9, 7, 6, 1));;
     }
 
 }
---
+++ java_programming/src/main/java/programmers/아이템_줍기/Test.java
@@ -0,0 +1,7 @@
+package programmers.아이템_줍기;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(2.5==((float)5/2));
+    }
+}
